dfa17d744327b938400e5f4b3d3b08b263c11684
ipc103 <iancandysemail@gmail.com>
2015-03-31 15:05:02 -0400

    update search page

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,15 @@
+class SearchController < ApplicationController
+
+  def index
+    @candidates = Candidate.find_by(zip_code: params[:q])
+    if @candidates.empty?
+      #hit the api
+      # for each result, update the zip code property of the candidates
+      #return the results of the updated objects
+    else
+      @candidates
+    end
+  end
+
+
+end
