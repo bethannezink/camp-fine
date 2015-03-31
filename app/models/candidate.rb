565f3c2a3227b392e6009f0a522021f5995ec6b6
ipc103 <iancandysemail@gmail.com>
2015-03-31 09:29:10 -0400

    build name method

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -2,4 +2,23 @@ class Candidate < ActiveRecord::Base
   has_many :contributions
   has_many :donors, through: :contributions
 
+
+  def party_name
+    case self.party
+    when "D"
+      "Democratic Party"
+    when "R"
+      "Republican Party"
+    else
+      self.party
+    end
+  end
+
+  def self.independents
+    self.all.collect do |can|
+      can if can.party!= "D" && can.party != "R"
+    end.flatten
+  end
+
+
 end
