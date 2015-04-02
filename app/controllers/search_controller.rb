1095d6bba15d9a09c7f82b0276e062d39cd9aabf
bethannezink <bethannezink@gmail.com>
2015-04-02 17:53:58 -0400

    solr changes, not working yet

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 class SearchController < ApplicationController
 
   def index
-
     @candidates = Sunlight.new.return_candidate_objects(params[:q])
     if @candidates.empty?
       @result = "No results found. Please try a different search."
@@ -13,10 +12,31 @@ class SearchController < ApplicationController
   end
 
   def name
-    @candidates = Candidate.where("name LIKE ?", "%#{params[:q]}%")
-    @result = params[:q]
+    @candidates = Candidate.search(:include => [:name]) do
+      keywords(params[:q])
+    end
     render "candidates/index"
   end
 
-
 end
+
+
+
+# Example:
+# class PostsController < ApplicationController
+#   def search
+#     @search = Post.search(:include => [:comments]) do
+#       keywords(params[:q])
+#     end
+#   end
+# end
+
+# @candidates = Candidate.where("name LIKE ?", "%#{params[:q]}%")
+# @result = params[:q]
+# render "candidates/index"
+
+    
+  # end
+
+
+# end
